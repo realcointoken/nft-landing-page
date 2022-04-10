9a830b6cc36330191bf2854526fe2216308ab9d4
ufogreys <45027262+ufogreys@users.noreply.github.com>
2022-04-10 00:49:43 -0300

    Update countdown.js

--- js/countdown.js
+++ js/countdown.js
@@ -8,17 +8,11 @@ const countdownfunction = setInterval(function () {
   const now = new Date().getTime();
   const diff = countDownTime - now;
   const days = Math.floor(diff / (1000 * 60 * 60 * 24));
-  const hours = Math.floor(diff % (1000 * 60 * 60 * 24);
-  const minutes = Math.floor(diff % (1000 * 60 * 60);
-  const seconds = Math.floor(diff % (1000 * 60);
-
+  
   if (diff < 0) {
     clockdiv.style.display = "none";
     clearInterval(countdownfunction);
   } else {
     clockdiv.querySelector(".days").innerHTML = days;
-    clockdiv.querySelector(".hours").innerHTML = hours;
-    clockdiv.querySelector(".minutes").innerHTML = minutes;
-    clockdiv.querySelector(".seconds").innerHTML = seconds;
   }
 }
